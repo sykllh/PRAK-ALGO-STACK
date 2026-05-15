5a0e668929e50023dce893876231e3b1e8d4f875
sykllh <sykllhidmi@gmail.com>
2026-05-15 15:01:41 +0700

    membuat case 1 nya

--- stack.cpp
+++ stack.cpp
@@ -91,5 +91,11 @@ int main()
         cin >> choice;
 
         switch (choice)
+        {
+        case 1:
+            cout << "Enter the value to push: ";
+            cin >> value;
+            stack.push(value);
+            break;
 
 
